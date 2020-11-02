746b6a814f10af5fb6049a454a263e1d80b63586
むずでょ <muzudho1@gmail.com>
2020-11-03 02:40:25 +0900

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -6,23 +6,19 @@ pub mod expected_items;
 pub mod expected_items_builder;
 pub mod machine;
 
-pub struct AnyBuilder<T> {
-    items: Vec<T>,
-}
-
-pub struct Any<T> {
-    items: Vec<T>,
-}
-
 pub struct Machine {
     index: usize,
 }
 
-pub struct ExpectedItemsBuilder<T> {
+pub struct ActualItemsBuilder<T> {
     items: Vec<T>,
 }
 
-pub struct ActualItemsBuilder<T> {
+pub struct ActualItems<T> {
+    items: Vec<T>,
+}
+
+pub struct ExpectedItemsBuilder<T> {
     items: Vec<T>,
 }
 
@@ -30,6 +26,15 @@ pub struct ExpectedItems<T> {
     items: Vec<T>,
 }
 
-pub struct ActualItems<T> {
+pub enum ExpectedItem<T> {
+    Exact(T),
+    Any(Any<T>),
+}
+
+pub struct AnyBuilder<T> {
+    items: Vec<T>,
+}
+
+pub struct Any<T> {
     items: Vec<T>,
 }
